87a5f097425051321a92c5da6c280ee81a509114
MinzyChan <luuhoanganh9a8@gmail.com>
2026-08-13 13:41:36 +0700

    feat(error): hiển thị lỗi hệ thống trong ứng dụng

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import { AuthProvider } from './contexts/AuthContext';
+import ApiErrorRedirect from './components/shared/ApiErrorRedirect';
 
 const App = () => {
   return (
     // BrowserRouter bắt buộc phải nằm ngoài cùng để quản lý URL
     <BrowserRouter>
+      <ApiErrorRedirect />
       {/* Bọc AuthProvider ở đây để toàn bộ các Route và Component đều lấy được thông tin User */}
       <AuthProvider>
         <AppRouter />
