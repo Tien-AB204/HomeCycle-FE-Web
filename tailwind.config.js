afa7d2c79f9424a1b1131359980f667962ae89b9
Tien-AB204 <tientm2k3@gmail.com>
2026-09-08 14:20:03 +0700

    style(theme): tối ưu palette và style nền tảng

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        primary: 'var(--color-primary)',
+        border: 'var(--color-border)',
+        textLight: 'var(--color-textLight)',
+        error: 'var(--color-error)',
+        success: 'var(--color-success)',
+        warning: 'var(--color-warning)',
+        background: 'var(--color-background)',
+        white: 'var(--color-white)',
+      },
+    },
+  },
+};
