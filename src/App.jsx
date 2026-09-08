963bd9d27cb355505bb8f7ed742f0cd59c74ba5a
Tien-AB204 <tientm2k3@gmail.com>
2026-09-08 14:20:04 +0700

    feat(i18n): áp dụng locale tiếng Việt toàn cục

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { ConfigProvider } from 'antd';
+
+import viVN from "antd/locale/vi_VN";
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import { AuthProvider } from './contexts/AuthContext';
@@ -7,11 +10,20 @@ const App = () => {
   return (
     // BrowserRouter bắt buộc phải nằm ngoài cùng để quản lý URL
     <BrowserRouter>
-      <ApiErrorRedirect />
-      {/* Bọc AuthProvider ở đây để toàn bộ các Route và Component đều lấy được thông tin User */}
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
+      <ConfigProvider locale={viVN}
+        theme={{
+          token: {
+            colorPrimary: '#2B5659',
+            colorBgBase: '#F8F9FA',
+          },
+        }}
+      >
+        <ApiErrorRedirect />
+        {/* Bọc AuthProvider ở đây để toàn bộ các Route và Component đều lấy được thông tin User */}
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </ConfigProvider>
     </BrowserRouter>
   );
 };
